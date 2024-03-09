5fed9f4f71e21d906e188605ab5396e7281a6f10
Alejandro Portilla Gainza <aportiga@mercadona.es>
2024-03-09 20:48:55 +0100

    feat: Solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,5 +1,10 @@
 package org.tfoc;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  * The class containing the solution to this exercise
  */
@@ -9,7 +14,15 @@ public class Solution {
             String s,
             String t) {
 
-        return false;
+        Map<String, Long> collectS = getLettersOcurrences(s);
+        Map<String, Long> collectT = getLettersOcurrences(t);
+
+        return collectS.equals(collectT);
+    }
+
+    private Map<String, Long> getLettersOcurrences(String value) {
+        return Arrays.stream(value.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,33 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 class SolutionTest {
 
-    @Test
-    void testSolution() {
-
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+    private static Stream<Arguments> provideAnagram() {
+        return Stream.of(
+                Arguments.of("anagram", "nagaram", true),
+                Arguments.of("rat", "car", false),
+                Arguments.of("", "", true),
+                Arguments.of("U+0007raU+090x", "U+090xarU+0007", true)
+        );
     }
 
+    @ParameterizedTest
+    @MethodSource("provideAnagram")
+    void testSolution(String s, String t, Boolean resExpected) {
+        Solution solution = new Solution();
+
+        solution.isAnagram(s, t);
+
+        assertEquals(solution.isAnagram(s, t), resExpected);
+    }
 }
